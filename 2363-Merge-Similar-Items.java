fa1408f438ddb8900687cfc5cd974fd40db21038
Vibishraj-Senthilkumar <vibishrajsenthil@gmail.com>
2024-07-30 09:58:32 +0530

    [2363] [Time Beats: 24.27%] [Memory Beats: 99.59%] - LeetPush

---
+++ 2363-Merge-Similar-Items.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
+        TreeMap<Integer,Integer> tm=new TreeMap<Integer,Integer>() ;
+        for(int i=0;i<items1.length;i++)
+        {
+                tm.put(items1[i][0],tm.getOrDefault(items1[i][0],0)+items1[i][1]);
+        }
+        for(int i=0;i<items2.length;i++)
+        {
+                tm.put(items2[i][0],tm.getOrDefault(items2[i][0],0)+items2[i][1]);
+        }
+        List<List<Integer>> list = new ArrayList<>();
+        for(int key:tm.keySet())
+        {   List<Integer> li=new ArrayList<>();
+            li.add(key);
+            li.add(tm.get(key));
+            list.add(li);
+        }
+        return list;
+    }
+}
