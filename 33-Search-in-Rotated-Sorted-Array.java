c30a7691669509412824bce48e76798d7576b1b7
Vibishraj-Senthilkumar <vibishrajsenthil@gmail.com>
2024-07-30 15:56:59 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 74.99%] - LeetPush

--- 33-Search-in-Rotated-Sorted-Array.java
+++ 33-Search-in-Rotated-Sorted-Array.java
@@ -1,33 +1,28 @@
 class Solution {
     public int search(int[] nums, int k) {
-        
-        int low =0; int high = nums.length-1;
-
-        while (low<=high){
-
-            int mid = low+ (high-low)/2;
-
-            if(nums[mid]==k) return mid;
-            else if(nums[low]<=nums[mid]){
-                
-                if(k>=nums[low]&&k<=nums[mid]){
-
-                    high = mid-1;
-                }
-                else{
-                    low = mid+1;
-                }
+        return bs(nums,0,nums.length-1,k);
+    }
+    public int bs(int[] nums,int left,int right,int k)
+    {
+        while(left<=right)
+        {
+            int mid=left+right-left/2;
+            if(nums[mid]==k)
+                return mid;
+            else if(nums[left]<=nums[mid])
+            {
+                if(k>=nums[left]&&k<=nums[mid])
+                    right=mid-1;
+                else
+                    left=mid+1;
             }
-            else{
-
-                if(k>=nums[mid] && k<=nums[high]){
-                    low = mid+1;
-                }
-                else{
-                    high = mid-1;
-                }
+            else 
+            {
+                if(k>=nums[mid]&&nums[right]>=k)
+                        left=mid+1;
+                else
+                     right=mid-1;
             }
-
         }
         return -1;
     }
