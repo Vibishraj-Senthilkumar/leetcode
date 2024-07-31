690900e8eb15016f81097bcb99308b665a57ad09
Vibishraj-Senthilkumar <vibishrajsenthil@gmail.com>
2024-07-31 12:03:27 +0530

    [69] [Time Beats: 85.92%] [Memory Beats: 23.42%] - LeetPush

--- 69-Sqrt(x).java
+++ 69-Sqrt(x).java
@@ -1,5 +1,25 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        return bs(0,x,x);
     }
+    \t int bs(long low,long high,long x)
+\t {  
+\t     while(low<=high)
+\t     {
+\t         long mid=low+(high-low)/2;
+\t         if(mid*mid==x)
+\t         {
+\t             return (int)mid;
+\t         }
+\t         else if(mid*mid<x)
+\t         {
+\t             low=mid+1;
+\t         }
+\t         else
+\t         {
+\t             high=mid-1;
+\t         }
+\t     }
+\t     return (int)high;
+}
 }
