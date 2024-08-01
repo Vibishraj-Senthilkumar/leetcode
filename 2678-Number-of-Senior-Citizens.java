59d463b91a564dc98395c56a8da43aed03e6f301
Vibishraj-Senthilkumar <vibishrajsenthil@gmail.com>
2024-08-01 09:41:33 +0530

    [2678] [Time Beats: 82.64%] [Memory Beats: 68.62%] - LeetPush

---
+++ 2678-Number-of-Senior-Citizens.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int c=0;
+        for(String i: details)
+        {
+            if(Integer.parseInt(i.substring(11,13))>60)
+                c++;
+        }
+        return c;
+    }
+}
