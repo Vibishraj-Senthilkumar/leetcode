009e29642c172c154b41784f626091e99240204b
Vibishraj-Senthilkumar <vibishrajsenthil@gmail.com>
2024-08-03 07:54:46 +0530

    [121] [Time Beats: 70.21%] [Memory Beats: 5.06%] - LeetPush

--- 121-Best-Time-to-Buy-and-Sell-Stock.java
+++ 121-Best-Time-to-Buy-and-Sell-Stock.java
@@ -4,7 +4,8 @@ class Solution {
         int a=nums[0];
         for(int i=0;i<nums.length-1;i++)
         {
-            a=Math.min(a,nums[i]);
+            if(a>nums[i])
+                a=nums[i];
             if(max<(nums[i+1])-a)
             max=nums[i+1]-a;
         }
