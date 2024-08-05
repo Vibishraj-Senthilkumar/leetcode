8bca2633329859ee02666078a19054640431be45
Vibishraj-Senthilkumar <vibishrajsenthil@gmail.com>
2024-08-05 15:33:16 +0530

    [796] [Time Beats: 100.00%] [Memory Beats: 60.08%] - LeetPush

--- 796-Rotate-String.java
+++ 796-Rotate-String.java
@@ -2,17 +2,19 @@ class Solution {
     public boolean rotateString(String s, String goal) {
         if(goal.length()!=s.length())
             return false;
-        StringBuffer s1=new StringBuffer(goal);
-        for(int i=1;i<=s.length();i++)
-        {
-            if(String.valueOf(s1).equals(s))
-            return true;
-            else
-            {   
-                s1.insert(0,s1.substring(s1.length()-1));
-                s1.delete(s1.length()-1,s1.length());
-            }
-        }
-        return false;
+        // StringBuffer s1=new StringBuffer(goal);
+        // for(int i=1;i<=s.length();i++)
+        // {
+        //     if(String.valueOf(s1).equals(s))
+        //     return true;
+        //     else
+        //     {   
+        //         s1.insert(0,s1.substring(s1.length()-1));
+        //         s1.delete(s1.length()-1,s1.length());
+        //     }
+        // }
+        // return false;
+        s=s+s;
+        return s.contains(goal);
     }
 }
