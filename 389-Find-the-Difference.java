e076fb2823e965e8a8e1beeadd74a7d08d3a456f
Vibishraj-Senthilkumar <vibishrajsenthil@gmail.com>
2024-08-12 13:59:05 +0530

    [389] [Time Beats: 100.00%] [Memory Beats: 41.10%] - LeetPush

---
+++ 389-Find-the-Difference.java
@@ -0,0 +1,14 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int x=0;
+        for(char s1:s.toCharArray())
+        {
+            x^=s1;
+        }
+        for(char t1:t.toCharArray())
+        {
+            x^=t1;
+        }
+        return (char)x;
+    }
+}
