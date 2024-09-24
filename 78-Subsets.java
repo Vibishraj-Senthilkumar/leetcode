2f800b8c49677bede20ef231f47335d33747dc9a
Vibishraj-Senthilkumar <vibishrajsenthil@gmail.com>
2024-09-24 10:09:42 +0530

    [78] [Time Beats: 53.37%] [Memory Beats: 69.92%] - LeetPush

--- 78-Subsets.java
+++ 78-Subsets.java
@@ -1,16 +1,16 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        int n=nums.length;
-        for(int i=0;i<(1<<n);i++)
-        {
-            List<Integer>li=new ArrayList<>();
-            for(int j=0;j<n;j++){
-                if((i & (1<<j)) >0)
-                    li.add(nums[j]);
-            }
-            ans.add(li);
+        List<List<Integer>> result=new ArrayList<>();
+        backtrack(nums,0,new ArrayList<>(),result);
+        return result;
+    }
+    public void backtrack(int[] nums,int start,List<Integer>li,List<List<Integer>> result)
+    {
+        result.add(new ArrayList<>(li));
+        for(int i=start;i<nums.length;i++){
+            li.add(nums[i]);
+            backtrack(nums,i+1,li,result);
+            li.remove(li.size()-1);
         }
-        return ans;
     }
 }
