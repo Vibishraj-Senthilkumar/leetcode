d75d5a214e2c65961947b91d00c909c395ca50b6
Vibishraj-Senthilkumar <vibishrajsenthil@gmail.com>
2024-12-09 20:25:21 +0530

    [88] [Time Beats: 28.82%] [Memory Beats: 81.21%] - LeetPush

---
+++ 88-Merge-Sorted-Array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i,j=0;
+        for(i=m;i<nums1.length;i++)
+        {
+            nums1[i]=nums2[j++];
+        }
+        Arrays.sort(nums1);
+    }
+}
